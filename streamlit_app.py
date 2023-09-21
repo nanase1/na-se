82aefdea7d6d43ba38ce7f5ec44210c92223de99
nanase1 <144981135+nanase1@users.noreply.github.com>
2023-09-21 04:38:53 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,15 @@ import random
 st.title("おみくじアプリ")
 
 if st.button("おみくじを引く"):
-    results=["大吉","中吉","小吉","吉","凶","大凶"]
+    results=["大吉","中吉","小吉","吉","凶","大凶",]
+    result=random.choice(results)
+    st.write(f"結果:{result}")
+
+    
+# タイトルを設定
+st.title("今日する教科")
+
+if st.button("さあ、選べ、、、"):
+    results=["古文","論国","数Ⅱ","物理","公共","地理","化学","数Ｂ"]
     result=random.choice(results)
     st.write(f"結果:{result}")
